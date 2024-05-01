3e364dd0430337e93da6c3ec2c15a23129a7ed99
fiyosa <fiyosa97@gmail.com>
2024-05-01 17:44:06 +0700

    new feature

--- src/config/secret.ts
+++ src/config/secret.ts
@@ -1,6 +1,7 @@
 const secret = {
   APP_NAME: process.env.APP_NAME,
   APP_MODE: process.env.APP_MODE,
+  APP_PORT: process.env.APP_PORT,
 
   SERVER_TIMEOUT: 1000 * 30, // miliseconds
   SERVER_URL: process.env.SERVER_URL,
